076b4119590c5dc53ddd04822afd0a0bcb28fc0c
Arup Saha <arups@zeta.tech>
2020-07-20 01:02:25 +0530

    added the Pod files

---
+++ ASDeviceModel/Classes/ASDeviceModel.h
@@ -0,0 +1,17 @@
+//
+//  ASDeviceModel.h
+//  ASDeviceModel
+//
+//  Created by arup.s on 19/07/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ASDeviceModel : NSObject
++ (NSString *) deviceHardwareModel;
++ (NSString *) deviceModelName;
+@end
+
+NS_ASSUME_NONNULL_END
